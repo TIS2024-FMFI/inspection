1a56b9489fca1abded19d432d60fbe35286a1e6c
RichardMacko <macko77@uniba.sk>
2024-12-31 13:55:48 +0100

    nearly completed version of barcode scanner

--- src/resources/get_data.php
+++ src/resources/get_data.php
@@ -12,9 +12,10 @@ try {
     if ($results) {
         echo json_encode($results); // Vráť výsledky ako JSON
     } else {
-        //pridať button: add to personalized list a ak na neho klikne tak sa pridá do neho a aj do databázy
+        http_response_code(404); // Set HTTP status code to 404
         echo json_encode(['error' => 'Produkt nebol nájdený.']);
     }
 } catch (PDOException $e) {
+    http_response_code(500); // Set HTTP status code to 500 for server errors
     echo json_encode(['error' => 'Chyba pri získavaní údajov: ' . $e->getMessage()]);
 }
